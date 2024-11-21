0ac47bb46bf5fa62d14015ff07e326577e731bf9
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-11-21 14:17:41 -0600

    fix: Cake chart updated

--- backend_tdr/data_containers/spendings.py
+++ backend_tdr/data_containers/spendings.py
@@ -1,31 +1,53 @@
-import pandas as pd
 from dash import dcc, html
 import plotly.express as px
 
 
 def cost_distribution(data):
-    fig_cost_distribution = px.pie(data, values="TotalAmount", names="RepairReason")
+    data = data.copy()
+    # Elimina los primeros 9 caracteres (Codigo)
+    data["JobCode"] = data["JobCode"].str[9:]
 
+    # Genera el gráfico de pastel
+    fig_cost_distribution = px.pie(
+        data,
+        values="TotalAmount",
+        names="JobCode",
+        color="JobCode",
+    )
+
+    # Configuración de las trazas
     fig_cost_distribution.update_traces(
         textposition="inside",
         textinfo="percent+label",
         insidetextorientation="radial",
+        hoverinfo="label+percent+value",  # Muestra porcentaje y valor absoluto
+
+        # Plantilla personalizada para el hover
+        hovertemplate="<b>%{label}</b><br>Porcentaje: %{percent}<br>Total: $%{value}<extra></extra>",
     )
 
+    # Configuración de diseño
     fig_cost_distribution.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),
         paper_bgcolor="rgba(0,0,0,0)",
         plot_bgcolor="rgba(0,0,0,0)",
+        title="Distribución de Gastos por Motivo de Reparación",
+        title_x=0.5,
         uniformtext_minsize=12,
         uniformtext_mode="hide",
     )
+
+    # Devuelve el Div con el gráfico
     return html.Div(
         [
             dcc.Graph(
                 id="cost-distribution-graph",
                 figure=fig_cost_distribution,
+
                 config={"displayModeBar": False},
-                style={"height": "89vh", "width": "97vw"},
+
+                style={"height": "100%", "width": "100%"},
             )
-        ]
+        ],
+        style={"height": "89vh", "width": "97vw"},  # Ajuste del tamaño del Div
     )
